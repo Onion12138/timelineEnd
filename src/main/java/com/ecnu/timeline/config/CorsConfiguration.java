297746cb74e40e586eedead2b790a2f87ee4b479
T_sky <744673725@qq.com>
2019-11-14 10:09:16 +0800

    跨域配置

---
+++ src/main/java/com/ecnu/timeline/config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.ecnu.timeline.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedHeaders("*");
+    }
+}
